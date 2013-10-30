b89192a8ec1bfe9c2003065af3eb83193cdcb444
Thomas Schorb <scth1028(at)hs-karlsruhe.de>
2013-10-30 13:37:20 +0100

    Kunde: Typ (Enum), Anfang Validierung

--- webshop/src/main/java/de/webshop/kundenverwaltung/domain/Kunde.java
+++ webshop/src/main/java/de/webshop/kundenverwaltung/domain/Kunde.java
@@ -1,11 +1,28 @@
 package de.webshop.kundenverwaltung.domain;
 
+import java.util.Date;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 //Test
 public class Kunde {
-	
+	//TODO (Validations, Constructors, Get/Set)
 	private long	id;
+	@NotNull
+	@Size (min = 2, max = 32)
+	@Pattern (regexp = "[A-Z][a-z]+")
 	private String	name;
+	@NotNull
+	@Size (min = 2, max = 32)
+	@Pattern (regexp = "[A-Z][a-z]+")
 	private String	vorname;
+	private Date geburtstag;
+	private String passwort;
+	private String email;
+	private Kundentyp typ;
+	private boolean geloescht;
 	
 	public String getName() {
 		return name;
