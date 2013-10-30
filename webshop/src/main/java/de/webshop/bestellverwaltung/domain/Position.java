f6628b9f3668e9ac79ca0143ed024b20e6bf4053
David Stoewe <stda1025 (at) hs-karlsruhe.de>
2013-10-30 15:35:32 +0100

    Position erweitert, alle Packages vorhanden

--- webshop/src/main/java/de/webshop/bestellverwaltung/domain/Position.java
+++ webshop/src/main/java/de/webshop/bestellverwaltung/domain/Position.java
@@ -16,4 +16,81 @@ public class Position {
 	
 	private int		anzahl;
 	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public Artikel getArtikel() {
+		return artikel;
+	}
+	
+	public void setArtikel(Artikel artikel) {
+		this.artikel = artikel;
+	}
+	
+	public URI getArtikelUri() {
+		return artikelUri;
+	}
+	
+	public void setArtikelUri(URI artikelUri) {
+		this.artikelUri = artikelUri;
+	}
+	
+	public int getAnzahl() {
+		return anzahl;
+	}
+	
+	public void setAnzahl(int anzahl) {
+		this.anzahl = anzahl;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + anzahl;
+		result = prime * result + ((artikel == null) ? 0 : artikel.hashCode());
+		result = prime * result + ((artikelUri == null) ? 0 : artikelUri.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Position other = (Position) obj;
+		if (anzahl != other.anzahl)
+			return false;
+		if (artikel == null) {
+			if (other.artikel != null)
+				return false;
+		}
+		else if (!artikel.equals(other.artikel))
+			return false;
+		if (artikelUri == null) {
+			if (other.artikelUri != null)
+				return false;
+		}
+		else if (!artikelUri.equals(other.artikelUri))
+			return false;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Position [id=" + id + ", artikel=" + artikel + ", artikelUri=" + artikelUri
+				+ ", anzahl=" + anzahl + "]";
+	}
+	
 }
