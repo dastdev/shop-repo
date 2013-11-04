5a9d503058cf69363b7767c620d426623b7b0eb6
Mario Reinholdt <rema1039 (at) hs-karlsruhe.de>
2013-11-04 13:16:43 +0100

    Post und Put für Artikel

--- webshop/src/main/java/de/webshop/artikelverwaltung/domain/Artikel.java
+++ webshop/src/main/java/de/webshop/artikelverwaltung/domain/Artikel.java
@@ -63,15 +63,6 @@ public class Artikel {
 	}
 	
 	// Getter und Setter
-	
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
 	public void setParentID(Long parentID) {
 		this.parentID = parentID;
 	}
--- webshop/src/main/java/de/webshop/artikelverwaltung/rest/ArtikelResource.java
+++ webshop/src/main/java/de/webshop/artikelverwaltung/rest/ArtikelResource.java
@@ -1,11 +1,14 @@
 package de.webshop.artikelverwaltung.rest;
 
+import java.math.BigDecimal;
 import java.net.URI;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,6 +19,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import de.webshop.artikelverwaltung.domain.Artikel;
+import de.webshop.artikelverwaltung.domain.Artikel.Kategorie;
 import de.webshop.artikelverwaltung.service.ArtikelServiceMock;
 import de.webshop.util.rest.UriHelper;
 import static de.webshop.util.Constants.SELF_LINK;
@@ -47,6 +51,35 @@ public class ArtikelResource {
 		return Response.ok(artikel)
                        .links(getTransitionalLinks(artikel, uriInfo))
                        .build();
+	}
+	
+	@POST
+	// TODO wandle MOCK um in reale Methode
+	// TODO Rückgabetyp über Erfolg oder Artikel als Rückgabewert
+	public void createArtikel() {
+		Artikel artikel = new Artikel();
+		artikel.setID(2L);
+		artikel.setArtikelnummer("AEAE99AE");
+		artikel.setBezeichnung("Fahrradschlauch 26\" 255mm");
+		artikel.setKurzBeschreibung("Mit diesem Fahrradschlauch haben die Pannen endlich ein Ende...");
+		artikel.setBeschreibung("Der Ultra Duba Mega Schlauch 26\" mit Pannenstoppeffekt macht dem Ärger endlich ein Ende, ...");
+		artikel.setKategorie(Kategorie.ERSATZTEILE);
+		artikel.setLagerbestand(70);
+		artikel.setPreis(new BigDecimal(14.99));
+	}
+	
+	@PUT
+	//TODO wandle MOCK in reale Methode um
+	public Artikel updateArtikel(Artikel alt) {
+		alt.setID(2L);
+		alt.setArtikelnummer("99AA99AA");
+		alt.setBezeichnung("Geupdatetes Produkt");
+		alt.setKurzBeschreibung("Dieses Produkt wurde geupdatet.");
+		alt.setBeschreibung("Lange Beschreibung eines aktualisierten Produktes.");
+		alt.setKategorie(Kategorie.ERSATZTEILE);
+		alt.setLagerbestand(12000);
+		alt.setPreis(new BigDecimal(99.99));
+		return alt;
 	}
 	
 	private Link[] getTransitionalLinks(Artikel artikel, UriInfo uriInfo) {
