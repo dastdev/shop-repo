010d826f4ad842852f2638b347cd2f5f86fc0e5f
Stephan-PC <Stephan-PC@10.116.37.144>
2013-11-05 10:02:18 +0100

    utf8 für ß bei straße

--- webshop/src/main/java/de/webshop/kundenverwaltung/domain/Adresse.java
+++ webshop/src/main/java/de/webshop/kundenverwaltung/domain/Adresse.java
@@ -7,89 +7,89 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class Adresse {
-	
+
 	// Eigenschaften
 	@Min(1)
-	private Long	id;
-	private String	straﬂe;
+	private Long id;
+	private String stra\u00DFe;
 	@Size(max = 4)
 	@Pattern(regexp = "\\d+")
-	private String	hausnummer;
+	private String hausnummer;
 	@Pattern(regexp = "\\d{4,5}")
-	private String	plz;
-	private String	stadt;
-	private Land	land;
-	
+	private String plz;
+	private String stadt;
+	private Land land;
+
 	// L‰nderk¸rzel nach ISO 3166
 	public enum Land {
-			AT,
-			CH,
-			DE
+		AT, CH, DE
 	}
-	
+
 	// get/set-Methoden
 	public Long getID() {
 		return id;
 	}
-	
+
 	public void setID(Long id) {
 		this.id = id;
 	}
-	
-	public String getStraﬂe() {
-		return straﬂe;
+
+	public String getStra\u00DFe() {
+		return stra\u00DFe;
 	}
-	
-	public void setStraﬂe(String straﬂe) {
-		this.straﬂe = straﬂe;
+
+	public void setStra\u00DFe(String stra\u00DFe) {
+		this.stra\u00DFe = stra\u00DFe;
 	}
-	
+
 	public String getHausnummer() {
 		return hausnummer;
 	}
-	
+
 	public void setHausnummer(String hausnummer) {
 		this.hausnummer = hausnummer;
 	}
-	
+
 	public String getPlz() {
 		return plz;
 	}
-	
+
 	public void setPlz(String plz) {
 		this.plz = plz;
 	}
-	
+
 	public String getStadt() {
 		return stadt;
 	}
-	
+
 	public void setStadt(String stadt) {
 		this.stadt = stadt;
 	}
-	
+
 	public Land getLand() {
 		return land;
 	}
-	
+
 	public void setLand(Land land) {
 		this.land = land;
 	}
-	
+
 	// Basismethoden
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((hausnummer == null) ? 0 : hausnummer.hashCode());
+		result = prime * result
+				+ ((hausnummer == null) ? 0 : hausnummer.hashCode());
 		result = prime * result + (int) (id ^ (id >>> 32));
 		result = prime * result + ((land == null) ? 0 : land.hashCode());
 		result = prime * result + ((plz == null) ? 0 : plz.hashCode());
 		result = prime * result + ((stadt == null) ? 0 : stadt.hashCode());
-		result = prime * result + ((straﬂe == null) ? 0 : straﬂe.hashCode());
+		result = prime * result
+				+ ((stra\u00DFe == null) ? 0 : stra\u00DFe.hashCode());
 		return result;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -102,8 +102,7 @@ public class Adresse {
 		if (hausnummer == null) {
 			if (other.hausnummer != null)
 				return false;
-		}
-		else if (!hausnummer.equals(other.hausnummer))
+		} else if (!hausnummer.equals(other.hausnummer))
 			return false;
 		if (id != other.id)
 			return false;
@@ -112,27 +111,25 @@ public class Adresse {
 		if (plz == null) {
 			if (other.plz != null)
 				return false;
-		}
-		else if (!plz.equals(other.plz))
+		} else if (!plz.equals(other.plz))
 			return false;
 		if (stadt == null) {
 			if (other.stadt != null)
 				return false;
-		}
-		else if (!stadt.equals(other.stadt))
+		} else if (!stadt.equals(other.stadt))
 			return false;
-		if (straﬂe == null) {
-			if (other.straﬂe != null)
+		if (stra\u00DFe == null) {
+			if (other.stra\u00DFe != null)
 				return false;
-		}
-		else if (!straﬂe.equals(other.straﬂe))
+		} else if (!stra\u00DFe.equals(other.stra\u00DFe))
 			return false;
 		return true;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Adresse [id=" + id + ", straﬂe=" + straﬂe + ", hausnummer=" + hausnummer + ", plz="
-				+ plz + ", stadt=" + stadt + ", land=" + land + "]";
+		return "Adresse [id=" + id + ", stra\u00DFe=" + stra\u00DFe
+				+ ", hausnummer=" + hausnummer + ", plz=" + plz + ", stadt="
+				+ stadt + ", land=" + land + "]";
 	}
 }
