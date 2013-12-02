660c9299f3b81e5965709b4a21fe492d775431e9
Stephan-PC <Stephan-PC@Stephan>
2013-12-02 14:53:14 +0100

    Internationaliserung Artikelpreishistorie

--- webshop/src/main/java/de/webshop/artikelverwaltung/domain/Artikelpreishistorie.java
+++ webshop/src/main/java/de/webshop/artikelverwaltung/domain/Artikelpreishistorie.java
@@ -18,75 +18,82 @@ import javax.xml.bind.annotation.XmlRootElement;
  */
 @XmlRootElement
 public class Artikelpreishistorie implements Serializable {
-	
+
 	private static final long serialVersionUID = -2471255330875233292L;
-	
+
 	// Eigenschaften
-	@NotNull
+	@NotNull(message = "{artikelverwaltung.artikelpreishistorie.id.notNull}")
 	@Min(1)
-	private long		id;
-	@NotNull
+	// FIXME: (1, message = "{artikelverwaltung.artikelpreishistorie.id.min}" )
+	private Long id;
+	@NotNull(message = "{artikelverwaltung.artikelpreishistorie.artikelID.notNul}")
 	@Min(1)
-	private long		artikelID;
-	@NotNull
-	private Date		gueltigVon;
-	@NotNull
+	// FIXME: (1, message =
+	// "{artikelverwaltung.artikelpreishistorie.artikelID.min}" )
+	private Long artikelID;
+	@NotNull(message = "{artikelverwaltung.artikelpreishistorie.gueltigVon.notNull}")
+	private Date gueltigVon;
+	@NotNull(message = "{artikelverwaltung.artikelpreishistorie.preis.notNull}")
 	@DecimalMin("0.0")
-	private BigDecimal	preis;
-	
+	// FIXME: ("0.0",message =
+	// "{artikelverwaltung.artikelpreishistorie.preis.decimalMin}")
+	private BigDecimal preis;
+
 	// Konstruktor
-	public Artikelpreishistorie(long artikelid, Date gueltigVon, BigDecimal preis) {
+	public Artikelpreishistorie(Long artikelid, Date gueltigVon,
+			BigDecimal preis) {
 		super();
 		this.artikelID = artikelid;
 		this.gueltigVon = gueltigVon;
 		this.preis = preis;
 	}
-	
+
 	// get/set-Methoden
-	public long getID() {
+	public Long getID() {
 		return id;
 	}
-	
-	public void setID(long id) {
+
+	public void setID(Long id) {
 		this.id = id;
 	}
-	
-	public long getArtikelid() {
+
+	public Long getArtikelid() {
 		return artikelID;
 	}
-	
-	public void setArtikelid(long artikelid) {
+
+	public void setArtikelid(Long artikelid) {
 		this.artikelID = artikelid;
 	}
-	
+
 	public Date getGueltigVon() {
 		return gueltigVon;
 	}
-	
+
 	public void setGueltigVon(Date gueltigVon) {
 		this.gueltigVon = gueltigVon;
 	}
-	
+
 	public BigDecimal getPreis() {
 		return preis;
 	}
-	
+
 	public void setPreis(BigDecimal preis) {
 		this.preis = preis;
 	}
-	
+
 	// BasisMethoden
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + (int) (artikelID ^ (artikelID >>> 32));
-		result = prime * result + ((gueltigVon == null) ? 0 : gueltigVon.hashCode());
+		result = prime * result
+				+ ((gueltigVon == null) ? 0 : gueltigVon.hashCode());
 		result = prime * result + (int) (id ^ (id >>> 32));
 		result = prime * result + ((preis == null) ? 0 : preis.hashCode());
 		return result;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -101,23 +108,21 @@ public class Artikelpreishistorie implements Serializable {
 		if (gueltigVon == null) {
 			if (other.gueltigVon != null)
 				return false;
-		}
-		else if (!gueltigVon.equals(other.gueltigVon))
+		} else if (!gueltigVon.equals(other.gueltigVon))
 			return false;
 		if (id != other.id)
 			return false;
 		if (preis == null) {
 			if (other.preis != null)
 				return false;
-		}
-		else if (!preis.equals(other.preis))
+		} else if (!preis.equals(other.preis))
 			return false;
 		return true;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Artikelpreishistorie [id=" + id + ", artikelid=" + artikelID + ", gueltigVon="
-				+ gueltigVon + ", preis=" + preis + "]";
+		return "Artikelpreishistorie [id=" + id + ", artikelid=" + artikelID
+				+ ", gueltigVon=" + gueltigVon + ", preis=" + preis + "]";
 	}
 }
