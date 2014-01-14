cd9bf0f9754b6d122eb4928e6456363940f6c033
Mario Reinholdt <rema1039 (at) hs-karlsruhe.de>
2014-01-14 14:43:20 +0100

    Load.sql : Artikel angepasst

---
+++ webshop/src/main/java/de/webshop/artikelverwaltung/service/ArtikelnummerExistsException.java
@@ -0,0 +1,24 @@
+package de.webshop.artikelverwaltung.service;
+
+import java.io.Serializable;
+
+public class ArtikelnummerExistsException extends ArtikelServiceException implements Serializable{
+
+	private static final long serialVersionUID = 1276693990289060330L;
+	private static final String MESSAGE_KEY = "artikel.artikelnummerExists";
+	private final String artikelnummer;
+	
+	public ArtikelnummerExistsException(String artikelnummer) {
+		super("Die Artikelnummer " + artikelnummer + " existiert bereits");
+		this.artikelnummer = artikelnummer;
+	}
+
+	public String getArtikelnummer() {
+		return artikelnummer;
+	}
+
+	@Override
+	public String getMessageKey() {
+		return MESSAGE_KEY;
+	}
+}
